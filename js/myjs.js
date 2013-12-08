1e04e249daefb4fd2449dbbadd95b45bd2fbd9ab
shashazhang <eileenss8824@gmail.com>
2013-12-07 14:59:45 -1200

    [ss] use d3 to genenrate data bar.

--- js/myjs.js
+++ js/myjs.js
@@ -1,4 +1,12 @@
-var body = d3.select("body");
-var div = body.append("div");
-div.html("Hello World!!!!!");
-div.style("color","black").style("background-color","steelblue");
+var data = [4, 8, 15, 16, 23, 42];
+
+var x = d3.scale.linear()
+    .domain([0, d3.max(data)])
+    .range([0, 420]);
+
+d3.select(".chart")
+    .selectAll("div")
+    .data(data)
+    .enter().append("div")
+    .style("width", function(d) { return x(d) + "px"; })
+    .text(function(d) { return d; });
